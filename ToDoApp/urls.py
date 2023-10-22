4cca4d2428fa771c1c729d5a7b435a3b2c4abed0
amirmahdi-alavi <alavi_amirmahdi@yahoo.com>
2023-10-22 21:30:31 +0330

    edited index url path

--- ToDoApp/urls.py
+++ ToDoApp/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('', include('index.urls'))
+    path('/', include('index.urls'))
 ]
 
 if settings.DEBUG == True:
